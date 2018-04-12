c6b713c6dc729fd57d0b0569b4de1b18f229f62b
dong.yang <dong.yang@roadefend.com>
2018-04-12 05:13:05 +0000

    add main and task for insightface

---
+++ main_insightface.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python3
+##  coding: utf-8 
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import sys
+
+def main():
+    pass
+    
+if __name__ == '__main__':
+    print(__file__)
+    main()
+    print('end')
+    exit()
